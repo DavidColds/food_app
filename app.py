dc687c1155a4050553d2e1e2ef56f9ad97cf9fdf
DavidColds <drughidavid6@gmail.com>
2019-07-15 17:20:44 +0200

    Connected FLask to Mongo.db

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'food_app'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-np4or.mongodb.net/food_app?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':

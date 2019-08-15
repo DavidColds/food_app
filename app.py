291ed33fed4ddb04cd3797f075b3621cdec99614
Drughi David <44336390+DavidColds@users.noreply.github.com>
2019-08-15 15:45:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'food_app'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-np4or.mongodb.net/food_app?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
